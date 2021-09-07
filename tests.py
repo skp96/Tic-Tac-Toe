620cd71a8cceae1ce0a229d6af72c8de90cd13bd
Sunny <patel.sunny293@gmail.com>
2021-09-07 17:59:03 -0400

    added package manager file, and set up testing enviornment

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TicTacToeTests(unittest.TestCase):
+
+    def test_testing_enviornment(self):
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
