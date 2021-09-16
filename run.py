8ca35867075fa7fb5a3b0806250af728b228e0c2
Sunny <patel.sunny293@gmail.com>
2021-09-16 16:45:42 -0400

    seperated test cases, updated execute_move method to return a single value, and extracted duplicated validtions into a reusable method

--- run.py
+++ run.py
@@ -1,23 +1,15 @@
 from core_tic_tac_toe.board import Board
 from core_tic_tac_toe.human_player import HumanPlayer
-import builtins
 
 def board_test_script():
-    input = builtins.input("Would you like to make a move to multiple positions: ")
     board = Board()
-    player = HumanPlayer("Ronald Graham", board)
+    player = HumanPlayer("Player 1", board)
 
-    if input.lower() == "yes":
-        player.make_move("1,1")
-        player.make_move("2,2")
-        player.make_move("3,3")
-        result = player.get_moves()
-        print(result)
-    elif input.lower() == "no":
-        result = player.get_moves()
-        print(result)
-    else:
-        board_test_script()
+    player.make_move("1,1")
+    player.make_move("2,2")
+    player.make_move("3,3")
+    result = board.retrieve_list_of_moves()
+    print(result)
 
 if __name__ == "__main__":
     board_test_script()
