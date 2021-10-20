82304b647c573c317a5954010141012bd2b59030
Sunny <patel.sunny293@gmail.com>
2021-10-20 09:07:09 -0400

    circleci check failed so corrected test case

--- tests/test_board.py
+++ tests/test_board.py
@@ -107,8 +107,8 @@ class TestBoard:
         assert result == [0, 1, 3, 5, 6, 7, 8]
 
     def test_get_horizontal_vertical_and_diagonal_positions(self, board):
-        positions = [[1, 2, 3], [4, 5, 6], [
-            7, 8, 9], [1, 4, 7, ], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                     [1, 4, 7, ], [2, 5, 8], [3, 6, 9]]
 
         result = board.get_all_positions()
 
